1fcec44dba1e8d8908b1ade9b30432c139018b04
Elton Bautista <eltonbestaha@gmail.com>
2022-07-13 08:19:38 -0600

    Adjusted some values

--- src/components/Introduction/PersonalDisplays.jsx
+++ src/components/Introduction/PersonalDisplays.jsx
@@ -3,7 +3,6 @@ import styles from "../../styles/style-modules/PersonalDisplays.module.css";
 
 const PersonalDisplays = (props) => {
   const { description, noun, videoSrc, active, activity, classes } = props;
-  console.log(classes);
   return (
     <div className={`${styles[classes]} ${"personal-display"}`} data-active={active} activity={activity}>
       <div className={` ${styles['noun-container']} ${"personal"}`}>

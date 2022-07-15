b8397506174d292afabaf1d2a36f436649a5039e
Elton Bautista <eltonbestaha@gmail.com>
2022-07-15 07:47:27 -0600

    Added nav links to anchors in navbar Edited some text content in PersonalDisplays for grammar purposes

--- src/utils.js
+++ src/utils.js
@@ -5,7 +5,7 @@ const allMyText = {
   descriptionOne: "Shorter description for developer",
 
   nounTwo: "powerlifter",
-  descriptionTwo: "141kg/310lb bench press at 70.5kg/155lb body-weight, this right here is my personal best! I enjoy pushing myself to the limits. No day has passed where I thought that my current state was good enough. ",
+  descriptionTwo: "141kg/310lb bench press at 70.5kg/155lb body-weight, this right here is my personal best (PB)! I enjoy pushing myself to the limits, and I am a strong believer that with effort and discipline we can achieve anything. Ask me about my squat and deadlift PBs!",
 
   nounThree: "hiker",
   descriptionThree: "Everything from preparing your hiking bag, or driving out to the mountains and enjoying the sceneries, and even all the way to the rigorous ascent up to the peak - I love it all.",
